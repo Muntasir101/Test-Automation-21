052f55353d6745a9b933c615cea855d251c10460
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-09-10 20:19:49 +0600

    TN invalid test cases

--- Tutorial_Ninja/login.py
+++ Tutorial_Ninja/login.py
@@ -58,4 +58,54 @@ def testCase_valid_1():
     driver.close()
 
 
-testCase_valid_1()
+def testCase_invalid_2():
+    # launch browser
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+
+    # step 1: Go to Application page
+    driver.get(config['home_page'])
+    time.sleep(3)
+
+    # My Account
+    my_account = driver.find_element(By.CSS_SELECTOR, ".list-inline .dropdown .hidden-md")
+    my_account.click()
+
+    login = driver.find_element(By.LINK_TEXT, "Login")
+    login.click()
+    time.sleep(5)
+
+    email = driver.find_element(By.CSS_SELECTOR, "#input-email")
+    email.clear()
+
+    registered_email = open("login_credentials.txt", "r").readline()
+    email.send_keys(registered_email)
+
+    password = driver.find_element(By.CSS_SELECTOR, "#input-password")
+    password.clear()
+    password.send_keys("1234567832324")
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "form[method='post'] > input[value='Login']")
+    login_button.click()
+
+    # Verification Login Failed
+    expected_error_message = "Warning: No match for E-Mail Address and/or Password."
+
+    actual_error_message = driver.find_element(By.CSS_SELECTOR, ".alert-dismissible").text
+
+    if expected_error_message == actual_error_message:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write("\nTest Case Passed. Error Message display for Invalid login.\n")
+    else:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write(
+                f"\nTest Case failed !!\n"
+                f"Getting Actual Result: {actual_error_message}\n"
+                f"Expected Result: {expected_error_message}\n"
+            )
+
+    driver.close()
+
+
+
+testCase_invalid_2()
--- Tutorial_Ninja/register_account.py
+++ Tutorial_Ninja/register_account.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 import json
 from faker import Faker
+
 fake = Faker()
 
 # Load configuration data from config.json file
@@ -11,7 +12,7 @@ with open('TN_Config.json', 'r') as file:
     config = json.load(file)
 
 
-def testCase_1():
+def testCase_valid_1():
     # launch browser
     driver = webdriver.Firefox()
     driver.maximize_window()
@@ -28,7 +29,7 @@ def testCase_1():
     register.click()
     time.sleep(5)
 
-    first_name = driver.find_element(By.CSS_SELECTOR,"#input-firstname")
+    first_name = driver.find_element(By.CSS_SELECTOR, "#input-firstname")
     first_name.clear()
     first_name.send_keys("Test First Name")
 
@@ -84,13 +85,150 @@ def testCase_1():
     driver.close()
 
 
-testCase_1()
+def testCase_firstName_2():
+    # launch browser
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+
+    # step 1: Go to Application page
+    driver.get(config['home_page'])
+    time.sleep(3)
+
+    # My Account
+    my_account = driver.find_element(By.CSS_SELECTOR, ".list-inline .dropdown .hidden-md")
+    my_account.click()
+
+    register = driver.find_element(By.LINK_TEXT, "Register")
+    register.click()
+    time.sleep(5)
+
+    first_name = driver.find_element(By.CSS_SELECTOR, "#input-firstname")
+    first_name.clear()
+    first_name.send_keys("")
+
+    last_name = driver.find_element(By.CSS_SELECTOR, "#input-lastname")
+    last_name.clear()
+    last_name.send_keys("Test Last Name")
+
+    unique_email = fake.email()
+    with open("login_credentials.txt", 'w') as file:
+        file.write(unique_email)
+
+    email = driver.find_element(By.CSS_SELECTOR, "#input-email")
+    email.clear()
+    email.send_keys(unique_email)
+
+    telephone = driver.find_element(By.CSS_SELECTOR, "#input-telephone")
+    telephone.clear()
+    telephone.send_keys("12345678")
+
+    password = driver.find_element(By.CSS_SELECTOR, "#input-password")
+    password.clear()
+    password.send_keys("12345678")
+
+    confirm_password = driver.find_element(By.CSS_SELECTOR, "#input-confirm")
+    confirm_password.clear()
+    confirm_password.send_keys("12345678")
+
+    newsletter_yes = driver.find_element(By.CSS_SELECTOR, "label:nth-of-type(1) > input[name='newsletter']")
+    newsletter_yes.click()
+
+    privacy_agree = driver.find_element(By.CSS_SELECTOR, "input[name='agree']")
+    privacy_agree.click()
+
+    continue_button = driver.find_element(By.CSS_SELECTOR, "input[value='Continue']")
+    continue_button.click()
+
+    # Verification error message for first name
+    expected_result = "First Name must be between 1 and 32 characters!"
+
+    actual_result = driver.find_element(By.CSS_SELECTOR, "#account .text-danger").text
+
+    if expected_result == actual_result:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write("\nError Message for First name display.Test Passed.\n")
+    else:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write(
+                f"\nError Message for First name failed !!\n"
+                f"Getting Actual Result: {actual_result}\n"
+                f"Expected Result: {expected_result}\n"
+            )
+
+    driver.close()
 
 
+def testCase_lastName_3():
+    # launch browser
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+
+    # step 1: Go to Application page
+    driver.get(config['home_page'])
+    time.sleep(3)
+
+    # My Account
+    my_account = driver.find_element(By.CSS_SELECTOR, ".list-inline .dropdown .hidden-md")
+    my_account.click()
+
+    register = driver.find_element(By.LINK_TEXT, "Register")
+    register.click()
+    time.sleep(5)
+
+    first_name = driver.find_element(By.CSS_SELECTOR, "#input-firstname")
+    first_name.clear()
+    first_name.send_keys("Test Name")
+
+    last_name = driver.find_element(By.CSS_SELECTOR, "#input-lastname")
+    last_name.clear()
+    last_name.send_keys("")
+
+    unique_email = fake.email()
+    with open("login_credentials.txt", 'w') as file:
+        file.write(unique_email)
+
+    email = driver.find_element(By.CSS_SELECTOR, "#input-email")
+    email.clear()
+    email.send_keys(unique_email)
+
+    telephone = driver.find_element(By.CSS_SELECTOR, "#input-telephone")
+    telephone.clear()
+    telephone.send_keys("12345678")
+
+    password = driver.find_element(By.CSS_SELECTOR, "#input-password")
+    password.clear()
+    password.send_keys("12345678")
+
+    confirm_password = driver.find_element(By.CSS_SELECTOR, "#input-confirm")
+    confirm_password.clear()
+    confirm_password.send_keys("12345678")
+
+    newsletter_yes = driver.find_element(By.CSS_SELECTOR, "label:nth-of-type(1) > input[name='newsletter']")
+    newsletter_yes.click()
+
+    privacy_agree = driver.find_element(By.CSS_SELECTOR, "input[name='agree']")
+    privacy_agree.click()
+
+    continue_button = driver.find_element(By.CSS_SELECTOR, "input[value='Continue']")
+    continue_button.click()
+
+    # Verification error message for last name
+    expected_result = "Last Name must be between 1 and 32 characters!"
+
+    actual_result = driver.find_element(By.CSS_SELECTOR, "#account .text-danger").text
+
+    if expected_result == actual_result:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write("\nError Message for Last name display.Test Passed.\n")
+    else:
+        with open("TN_test_result.txt", 'a') as file:
+            file.write(
+                f"\nError Message for Last name failed !!\n"
+                f"Getting Actual Result: {actual_result}\n"
+                f"Expected Result: {expected_result}\n"
+            )
+
+    driver.close()
 
 
-
-
-
-
-
+testCase_lastName_3()
